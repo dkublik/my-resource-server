642789a6f79b8766de983d53aec9610e9d5adb10
dawid.kublik@vimn.com <dawid.kublik@vimn.com>
2020-06-15 21:12:26 +0200

    initial

---
+++ src/main/java/pl/dk/resserver/ResserverApplication.java
@@ -0,0 +1,13 @@
+package pl.dk.resserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/dk/resserver/ResserverApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.dk.resserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
